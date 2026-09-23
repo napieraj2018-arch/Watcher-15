087dbd04aac43b55512435c68021ca1d6613d6a9
napieraj2018-arch <napieraj2018@gmail.com>
2026-09-23 09:55:20 +0200

    Repair Oasis probe import line

--- src/probe_oasis_confirm.py
+++ src/probe_oasis_confirm.py
@@ -1,4 +1,5 @@
-import json,time\nimport requests
+import json,time
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
